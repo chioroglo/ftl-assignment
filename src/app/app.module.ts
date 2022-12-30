21e2cf85c7f68510caeae973233dea403c0d2939
kioroglomobster@gmail.com <kioroglomobster@gmail.com>
2022-12-30 15:45:48 +0200

    (ui) added page components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { ProfilePageComponent } from './components/profile-page/profile-page.component';
+import { RouteNotFoundPageComponent } from './components/route-not-found-page/route-not-found-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    ProfilePageComponent,
+    RouteNotFoundPageComponent
   ],
   imports: [
     BrowserModule
